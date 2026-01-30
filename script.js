2d09985b7e5206dfcd64fe03a44a3bf9161dc6d8
goku-beep <rajdeepmandal436@gmail.com>
2026-01-30 18:08:54 +0530

    Update BASE_URL to include protocol

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "goku-beep-github-io.vercel.app";
+const BASE_URL = "https://goku-beep-github-io.vercel.app";
 let currentQueue = [];
 let currentIndex = -1;
 let playbackHistory = []; 
